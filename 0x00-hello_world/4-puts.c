435b2f68ea1626ed6768ff03ba6cac65db00970a
walabouguera <80923530+walabouguera@user.noreply.git.com>
2023-02-16 08:20:26 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programe that print a line withe puts function
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle
+");
+return (0);
+}
