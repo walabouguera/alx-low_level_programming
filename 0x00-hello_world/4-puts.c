80a50bd33c93c8b8388d37a6d88b8a50b4b786c4
walabouguera <80923530+walabouguera@user.noreply.git.com>
2023-02-16 08:45:07 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - print "Programming is like building a multilingual puzzle, follwed by a newline."
+ * main - print "Programming is like building a multilingual 
+ * puzzle, follwed by a newline."
  *  Return 0.
  */
 int main(void)
