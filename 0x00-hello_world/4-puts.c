57ed552481cef00bc9b94b3019b63fb09b3e52e8
walabouguera <80923530+walabouguera@user.noreply.git.com>
2023-02-17 07:21:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #incloud<studio.h>
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingal puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
