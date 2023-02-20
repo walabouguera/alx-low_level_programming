a0313639ea908ed5d49f5209a81810f1e1447038
walabouguera <80923530+walabouguera@user.noreply.git.com>
2023-02-20 09:17:17 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,12 @@
 int main(void)
 {
 int n;
-int m;
 
 for (n = 48; n <= 57; n++)
 {
 putchar(n);
-}
-for (m = 97; m <= 102; m++)
-{
-putchar(m);
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
