0490e9c86a1f05aa3739c8a296c1edc4d68dd2df
walabouguera <80923530+walabouguera@user.noreply.git.com>
2023-02-22 07:08:08 -0800

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabetin lowercase
+ * Return: Alwayse 0 (Seuccess)
+ */
+void print_alphabet(void)
+{
+int i;
+
+for (i =97; i <= 122; i++)
+_putchar(i);
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times.
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
